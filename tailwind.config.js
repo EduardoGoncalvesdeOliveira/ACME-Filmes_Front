7a113176332cc54cb6f97bf3ab419167ee2a3af8
Eduardo Gonçalves <eduardogdo28@gmail.com>
2024-03-05 17:01:09 -0300

    first try acme

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: { azul: '#FF0000', vermelho: '#4541FF' }
+        },
+    },
+    plugins: [],
+}
